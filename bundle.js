879feafd5ea87baa1158c22fcbd33151eb6713e3
Dylan <kite.d92@gmail.com>
2018-11-29 16:02:35 -0800

    input back to text

--- bundle.js
+++ bundle.js
@@ -1697,22 +1697,22 @@ function init(){
 
 function delReview(e){
     console.log(e.target.parentElement)
-    e.target.parentElement.innerHTML += confirmHTML()
-    addButtonListeners()
+    e.target.parentElement.innerHTML += confirmHTML('deny', 'negative', 'cancel', 'delete')
+    addButtonListeners(minimize, remove)
 }
 
-function confirmHTML(){
+function confirmHTML(type1, type2, text1, text2){
     return `
     <div class="confirmBox" >
         Are you sure about that?
-        <div class="ui black deny button">cancel</div>
-        <div class="ui negative button">delete</div>
+        <div id="button1" class="ui ${type1} button">${text1}</div>
+        <div id="button2" class="ui ${type2} button">${text2}</div>
     </div >`
 }
 
-function addButtonListeners(){
-    document.querySelector('.deny').addEventListener('click', minimize)
-    document.querySelector('.negative').addEventListener('click', remove)
+function addButtonListeners(fn1, fn2){
+    document.querySelector('#button1').addEventListener('click', fn1)
+    document.querySelector('#button2').addEventListener('click', fn2)
 }
 
 function minimize(){
@@ -1725,6 +1725,7 @@ function minimize(){
         )
     }, 0
     )
+    init()
 }
 
 function remove(){
@@ -1750,19 +1751,38 @@ function remove(){
 }
 
 function editReview(e){
-    console.log(e.target.parentElement)
+    const originalVals = textToInput(e)
+    e.target.parentElement.innerHTML += confirmHTML('deny', 'positive','cancel', 'update')
+    addButtonListeners(function(e){minimize2(e, originalVals)})
+}
+
+function minimize2(e, originalVals){
+    minimize()
+    inputToText(e, originalVals)
+}
+function textToInput(e){
     let contentH3 = e.target.parentElement.children[0].textContent
     let contentRating = e.target.parentElement.children[1].textContent
     let contentText = e.target.parentElement.children[2].textContent
-    console.log(contentH3)
     e.target.parentElement.children[0].innerHTML = `<input type="text" required value="${contentH3}">`
     e.target.parentElement.children[1].innerHTML = `<input type="text" required value="${contentRating}">`
-    e.target.parentElement.children[2].innerHTML = `<textarea required value="${contentText}"></textarea>`
-    // e.target.parentElement.children[2].children[0].textContent = contentText 
-    console.log(e.target.parentElement.children[2].children[0])
-
-
+    e.target.parentElement.children[2].innerHTML = `<textarea required value="${contentText}">${contentText}</textarea>`
+    const originalVals = {contentH3, contentRating, contentText}
+    return originalVals
 }
+
+function inputToText(e, { contentH3, contentRating, contentText }){
+    // console.log(e.target.parentElement.parentElement.children[0].children[0].value)
+    // let contentH3 = e.target.parentElement.parentElement.children[0].children[0].value
+    // let contentRating = e.target.parentElement.parentElement.children[1].children[0].value
+    // let contentText = e.target.parentElement.parentElement.children[2].children[0].value
+
+    e.target.parentElement.parentElement.children[0].innerHTML = contentH3
+    e.target.parentElement.parentElement.children[1].innerHTML = contentRating
+    e.target.parentElement.parentElement.children[2].innerHTML = contentText
+}
+
+
 module.exports = {init}
 
 },{"./alert":29,"axios":2}],32:[function(require,module,exports){
